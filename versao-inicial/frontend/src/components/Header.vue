004514460ea3c38304283e6872bef34f5ebf4371
luizpbello <luizpaulopinto25@gmail.com>
2022-04-27 08:11:30 -0300

    first commit

---
+++ versao-inicial/frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+
+<header class="header"></header>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header{
+    grid-area: header;
+    background-color: blueviolet;
+    display: flex;
+    justify-content: center;
+    align-self: center;
+}
+
+</style>
